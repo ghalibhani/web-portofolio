874a511bb9c72c3036b609f8e5cdeca72c6cc694
ghalib pratama <ghalibpratama.gpro@gmail.com>
2024-12-27 20:46:36 +0700

    refactor: restructure App component and remove unused assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import About from "./components/about/About";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/contact/Contact";
+import Home from "./components/home/Home";
+import Portofolio from "./components/portofolio/Portofolio";
+import Pricing from "./components/pricing/Pricing";
+import Resume from "./components/resume/Resume";
+import Service from "./components/services/Service";
+import Sidebar from "./components/sidebar/Sidebar";
+import Testimonial from "./components/testimonial/Testimonial";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sidebar />
+      <main>
+        <Home />
+        <About />
+        <Service />
+        <Resume />
+        <Portofolio />
+        <Pricing />
+        <Testimonial />
+        <Blog />
+        <Contact />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
