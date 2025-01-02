2c4f850311a83d5f1a1bf17f9b6952624768bb6c
ghalib pratama <ghalibpratama.gpro@gmail.com>
2025-01-02 14:56:07 +0700

    feat: add Portfolio component with filtering functionality and menu items

---
+++ src/components/portofolio/Menu.jsx
@@ -0,0 +1,47 @@
+import Work1 from "../../assets/work-1.svg";
+import Work2 from "../../assets/work-2.svg";
+import Work3 from "../../assets/work-3.svg";
+import Work4 from "../../assets/work-4.svg";
+import Work5 from "../../assets/work-5.svg";
+import Work6 from "../../assets/work-6.svg";
+
+const Menu = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Web Development",
+    category: "Web",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Mobile App Development",
+    category: "Mobile",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "API Development",
+    category: "API",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "Database Development",
+    category: "Web",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "Desktop Development",
+    category: "Mobile",
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "Testing",
+    category: "API",
+  },
+];
+
+export default Menu;
--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import Menu from "./Menu";
 
 function Portofolio() {
+  const [items, setItems] = useState(Menu);
+  const filterItem = (category) => {
+    const updatedItems = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+    setItems(updatedItems);
+  };
+
   return (
-    <div>Portofolio</div>
-  )
+    <section id='services' className='pt-40 flex flex-col justify-center px-36'>
+      <h2 className='font-bold text-5xl'>Recent Works</h2>
+      <div className='flex justify-center gap-4 mt-10'>
+        <span
+          className='bg-yellow-300 px-4 py-1 font-semibold rounded-xl cursor-pointer'
+          onClick={() => setItems(Menu)}
+        >
+          Everything
+        </span>
+        <span
+          className='bg-yellow-300 px-4 py-1 font-semibold rounded-xl cursor-pointer'
+          onClick={() => filterItem("Web")}
+        >
+          Web Developer
+        </span>
+        <span
+          className='bg-yellow-300 px-4 py-1 font-semibold rounded-xl cursor-pointer'
+          onClick={() => filterItem("API")}
+        >
+          API Developer
+        </span>
+        <span
+          className='bg-yellow-300 px-4 py-1 font-semibold rounded-xl cursor-pointer'
+          onClick={() => filterItem("Mobile")}
+        >
+          Mobile Developer
+        </span>
+      </div>
+      <div className='grid grid-cols-3 mt-8 gap-8'>
+        {items.map((item) => {
+          const { id, image, title, category } = item;
+          return (
+            <div
+              className='shadow-lg rounded-3xl p-6 relative overflow-hidden group'
+              key={id}
+            >
+              <div>
+                <img src={image} alt={title} />
+                <div className='bg-blue-500 absolute left-0 top-0 h-full w-full opacity-0 group-hover:opacity-80 transition-all'></div>
+              </div>
+              <span className='text-white font-semibold bg-purple-800 absolute top-0 left-10 px-4 py-2 rounded-b-2xl transform -translate-y-12 transition-all group-hover:opacity-100 group-hover:translate-y-0'>
+                {category}
+              </span>
+              <h3 className='text-4xl text-white px-2 absolute top-24 transform translate-y-10 transition-all font-semibold opacity-0 group-hover:opacity-100 group-hover:translate-y-0'>
+                {title}
+              </h3>
+              <a
+                className='text-white absolute text-2xl font-semibold bottom-6 bg-yellow-400 h-14 w-14 flex justify-center items-center cursor-pointer rounded-full transition-all opacity-0 group-hover:opacity-100'
+                href='#'
+              >
+                <i className='icon-link'></i>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default Portofolio
+export default Portofolio;
