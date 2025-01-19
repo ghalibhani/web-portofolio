9fe4cde0383ab59013b4c67a9eee4687b8d20771
Ghalib Hani <ghalibpratama.gpro@gmail.com>
2025-01-19 14:24:26 +0700

    fix text

--- src/components/about/AboutBox.jsx
+++ src/components/about/AboutBox.jsx
@@ -6,22 +6,22 @@ function AboutBox() {
       <div className='flex gap-4 items-center'>
         <i className='text-5xl text-gray-300 icon-fire'></i>
         <div>
-          <h3 className='font-bold text-2xl text-gray-700'>180</h3>
+          <h3 className='font-bold text-2xl text-gray-700'>10+</h3>
           <span className='font-semibold text-gray-700'>Project completed</span>
         </div>
       </div>
       <div className='flex gap-4 mt-5 md:mt-0 items-center'>
         <i className='text-5xl text-gray-300 icon-cup'></i>
         <div>
-          <h3 className='font-bold text-2xl text-gray-700'>180</h3>
+          <h3 className='font-bold text-2xl text-gray-700'>100+</h3>
           <span className='font-semibold text-gray-700'>Cup of Coffe</span>
         </div>
       </div>
       <div className='flex gap-4 mt-5 md:mt-0 items-center'>
         <i className='text-5xl text-gray-300 icon-people'></i>
         <div>
-          <h3 className='font-bold text-2xl text-gray-700'>180</h3>
-          <span className='font-semibold text-gray-700'>Cup of Coffe</span>
+          <h3 className='font-bold text-2xl text-gray-700'>10+</h3>
+          <span className='font-semibold text-gray-700'>Happy Clients</span>
         </div>
       </div>
     </div>
